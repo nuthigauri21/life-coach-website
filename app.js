d0a7b230ffc16d215456a43f18657ee3d7efe46f
nuthigauri21 <nuthi.gauri@gmail.com>
2026-07-08 23:36:23 +0530

    Add scroll-to-top button functionality

--- app.js
+++ app.js
@@ -91,3 +91,15 @@ modal.style.display="block";
 document.getElementById("closeModal").onclick=()=>{
 modal.style.display="none";
 }
+const topBtn=document.getElementById("topBtn");
+window.onscroll=()=>{
+topBtn.style.display=
+window.scrollY>200?
+"block":"none";
+};
+topBtn.onclick=()=>{
+window.scrollTo({
+top:0,
+behavior:"smooth"
+});
+};
